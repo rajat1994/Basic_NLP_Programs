f450547dfbb0537b7f2aa4ffa72a9591768749b2
rajat1994 <rajatarora25@yahoo.in>
2017-03-12 21:18:35 +0530

    first commit

---
+++ tokenizer.py
@@ -0,0 +1,11 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+example_text = "Hello Mr. Smith, how are doing today? The weather is great and python is awesome"
+
+print (sent_tokenize(example_text))
+
+print (word_tokenize(example_text))
+
+
+for i in  (word_tokenize(example_text)):
+	print(i)
